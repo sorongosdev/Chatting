1b1b4ce3ad83f59a4a247728e070bdbec42138ae
Sorongos <dosora9@gmail.com>
2023-03-13 00:48:11 +0900

    mypage에서 상태메시지 변경

--- app/src/main/java/com/sorongos/chatting/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sorongos/chatting/mypage/MyPageFragment.kt
@@ -7,16 +7,31 @@ import android.view.View
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.sorongos.chatting.Key
 import com.sorongos.chatting.LoginActivity
 import com.sorongos.chatting.R
 import com.sorongos.chatting.databinding.FragmentMypageBinding
+import com.sorongos.chatting.user.UserItem
 
 class MyPageFragment : Fragment(R.layout.fragment_mypage) {
     private lateinit var binding: FragmentMypageBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMypageBinding.bind(view)
+
+        //원래 기능 설정
+        val currentUserId = Firebase.auth.currentUser?.uid ?: ""
+        val currentUserDB = Firebase.database.reference.child(Key.DB_USERS).child(currentUserId)
+
+        currentUserDB.get().addOnSuccessListener {
+            val currentUserItem = it.getValue(UserItem::class.java) ?: return@addOnSuccessListener
+
+            binding.userNameEditText.setText(currentUserItem.username)
+            binding.userDescriptionEditText.setText(currentUserItem.description)
+
+        }
 
         binding.applyButton.setOnClickListener {
             val username = binding.userNameEditText.text.toString()
@@ -31,13 +46,17 @@ class MyPageFragment : Fragment(R.layout.fragment_mypage) {
                     }
                 }
             }
-
+            //firebase
+            val user = mutableMapOf<String, Any>()
+            user["username"] = username
+            user["description"] = description
+            currentUserDB.updateChildren(user)
         }
 
         /**로그아웃 하면 mainActivity 종료*/
         binding.signOutButton.setOnClickListener {
             Firebase.auth.signOut()
-            startActivity(Intent(context,LoginActivity::class.java))
+            startActivity(Intent(context, LoginActivity::class.java))
             activity?.finish()
         }
     }
