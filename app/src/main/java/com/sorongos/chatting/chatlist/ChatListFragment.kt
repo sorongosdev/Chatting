c5bbab6c3c923728f146afdb69d244bb113d631d
Sorongos <dosora9@gmail.com>
2023-03-13 00:48:24 +0900

    userList와 동일하게 적용

--- app/src/main/java/com/sorongos/chatting/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/sorongos/chatting/chatlist/ChatListFragment.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.sorongos.chatting.Key
 import com.sorongos.chatting.R
 import com.sorongos.chatting.databinding.FragmentChatroomlistBinding
 
@@ -18,11 +25,23 @@ class ChatListFragment : Fragment(R.layout.fragment_chatroomlist) {
             layoutManager = LinearLayoutManager(context)
             adapter = chatListAdapter
         }
+        val currentUserId = Firebase.auth.currentUser?.uid ?: return
+        val chatRoomsDB = Firebase.database.reference.child(Key.DB_CHAT_ROOMS).child(currentUserId)
 
-        chatListAdapter.submitList(
-            mutableListOf<ChatRoomItem?>().apply{
-                add(ChatRoomItem("11","22","33"))
+        //데이터가 바뀔때마다 수신
+        chatRoomsDB.addValueEventListener(object:ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val chatRoomList = snapshot.children.map{
+                    //변환
+                    it.getValue(ChatRoomItem::class.java)
+                }
+                chatListAdapter.submitList(chatRoomList)
             }
-        )
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
--- app/src/main/java/com/sorongos/chatting/chatlist/ChatRoomItem.kt
+++ app/src/main/java/com/sorongos/chatting/chatlist/ChatRoomItem.kt
@@ -1,7 +1,8 @@
 package com.sorongos.chatting.chatlist
 
 data class ChatRoomItem(
-    val chatRoomId: String,
-    val otherUserName: String,
-    val lastMessage: String
+    val chatRoomId: String? = null,
+    val otherUserName: String? = null,
+    val lastMessage: String? = null,
+    val otherUserId: String? = null
 )
